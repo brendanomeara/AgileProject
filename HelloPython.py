6e40972f67abb178b4a25bc86909b291fd4b5030
Brendan O'Meara <brendan.omeara@mycit.ie>
2019-11-14 10:38:38 +0000

    The second commit to the github repository

--- HelloPython.py
+++ HelloPython.py
@@ -1,5 +1,6 @@
 import datetime
 print("Hello world!")
+print("This is the second commit to the HelloPython file")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
