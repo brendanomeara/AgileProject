8c6c9b3195964e2c41366682df8ac3b93a096f12
Brendan O'Meara <brendan.omeara@mycit.ie>
2019-11-19 11:32:42 +0000

    this is the third commit

--- HelloPython.py
+++ HelloPython.py
@@ -2,6 +2,7 @@ import datetime
 print("Hello world!")
 print("This is the second commit to the HelloPython file")
 now = datetime.datetime.now()
+print("This is the third commit to the HelloPython file")
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
 
