ec4aa2681302b21895371ec579bfe7a8fc1a0e8a
Brendan O'Meara <brendan.omeara@mycit.ie>
2019-11-22 13:37:01 +0000

    This is the fourth commit

--- HelloPython.py
+++ HelloPython.py
@@ -5,6 +5,6 @@ now = datetime.datetime.now()
 print("This is the third commit to the HelloPython file")
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
-
+print("Wagwan mandem")
 
 	
