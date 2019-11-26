7980fd2b20e9e7e51931f709ca9981a09990a1e8
Brendan O'Meara <brendan.omeara@mycit.ie>
2019-11-26 09:47:14 +0000

    Buildkite successful

--- HelloPython.py
+++ HelloPython.py
@@ -4,7 +4,7 @@ print("This is the second commit to the HelloPython file")
 now = datetime.datetime.now()
 print("This is the third commit to the HelloPython file")
 print ("Current date and time is ")
-print (now.strftime("%A, %d-%m-%Y : %H:%M"))	
+print (now.strftime("%A, %d-%m-%Y : %H:%M")
 print("Wagwan mandem")
+print("BUildkite Successful")
 
-	
